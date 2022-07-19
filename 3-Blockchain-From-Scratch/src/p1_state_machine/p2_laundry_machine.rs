73a1ed5265aeefcb765807ce666129fe319dc18b
Frank Bell <fb@evilrobot.net>
2022-07-20 00:15:18 +0100

    Completed first two state machine activities

--- 3-Blockchain-From-Scratch/src/p1_state_machine/p2_laundry_machine.rs
+++ 3-Blockchain-From-Scratch/src/p1_state_machine/p2_laundry_machine.rs
@@ -1,7 +1,7 @@
 //! When you wear clothes they get dirty. When you wash them they get wet. When you dry them, they're
-//! ready to be worn again. Or course washing and wearing clothes takes its toll on the clothes, and
+//! ready to be worn again. Of course washing and wearing clothes takes its toll on the clothes, and
 //! eventually they get tattered.
-//! 
+//!
 use super::StateMachine;
 
 /// The rules are:
@@ -9,6 +9,7 @@ use super::StateMachine;
 pub struct ClothesMachine;
 
 /// Models a piece of clothing throughout its lifecycle.
+#[derive(PartialEq, Eq, Debug)]
 pub enum ClothesState {
     /// Clean clothes ready to be worn. With some given life left.
     Clean(u64),
@@ -28,71 +29,135 @@ pub enum ClothesAction {
 }
 
 impl StateMachine for ClothesMachine {
-
     type State = ClothesState;
     type Transition = ClothesAction;
 
     fn next_state(starting_state: &ClothesState, t: &ClothesAction) -> ClothesState {
-        todo!("Implement this state machine.")
+        match starting_state {
+            ClothesState::Clean(life) => match t {
+                ClothesAction::Wear => ClothesState::Dirty(life - 1),
+                ClothesAction::Wash => ClothesState::Wet(life - 2),
+                ClothesAction::Dry => ClothesState::Clean(life - 2),
+            },
+            ClothesState::Dirty(life) => match t {
+                ClothesAction::Wear => ClothesState::Dirty(life - 2),
+                ClothesAction::Wash => ClothesState::Wet(life - 1),
+                ClothesAction::Dry => ClothesState::Dirty(life - 3),
+            },
+            ClothesState::Wet(life) => match t {
+                ClothesAction::Wear => ClothesState::Dirty(life - 2),
+                ClothesAction::Wash => ClothesState::Wet(life - 2),
+                ClothesAction::Dry => ClothesState::Clean(life - 1),
+            },
+            ClothesState::Tattered => ClothesState::Tattered,
+        }
     }
 }
 
 #[test]
 fn sm_2_wear_clean_clothes() {
-
+    let starting_state = ClothesState::Clean(3);
+    assert_eq!(
+        ClothesMachine::next_state(&starting_state, &ClothesAction::Wear),
+        ClothesState::Dirty(2)
+    )
 }
 
 #[test]
 fn sm_2_wear_dirty_clothes() {
-
+    let starting_state = ClothesState::Dirty(3);
+    assert_eq!(
+        ClothesMachine::next_state(&starting_state, &ClothesAction::Wear),
+        ClothesState::Dirty(1)
+    )
 }
 
 #[test]
 fn sm_2_wear_wet_clothes() {
-
+    let starting_state = ClothesState::Dirty(3);
+    assert_eq!(
+        ClothesMachine::next_state(&starting_state, &ClothesAction::Wear),
+        ClothesState::Dirty(1)
+    )
 }
 
 #[test]
 fn sm_2_wear_tattered_clothes() {
-
+    let starting_state = ClothesState::Tattered;
+    assert_eq!(
+        ClothesMachine::next_state(&starting_state, &ClothesAction::Wear),
+        ClothesState::Tattered
+    )
 }
 
 #[test]
 fn sm_2_wash_clean_clothes() {
-
+    let starting_state = ClothesState::Clean(5);
+    assert_eq!(
+        ClothesMachine::next_state(&starting_state, &ClothesAction::Wash),
+        ClothesState::Wet(3)
+    )
 }
 
 #[test]
 fn sm_2_wash_dirty_clothes() {
-
+    let starting_state = ClothesState::Dirty(5);
+    assert_eq!(
+        ClothesMachine::next_state(&starting_state, &ClothesAction::Wash),
+        ClothesState::Wet(4)
+    )
 }
 
 #[test]
 fn sm_2_wash_wet_clothes() {
-
+    let starting_state = ClothesState::Wet(5);
+    assert_eq!(
+        ClothesMachine::next_state(&starting_state, &ClothesAction::Wash),
+        ClothesState::Wet(3)
+    )
 }
 
 #[test]
 fn sm_2_wash_tattered_clothes() {
-
+    let starting_state = ClothesState::Tattered;
+    assert_eq!(
+        ClothesMachine::next_state(&starting_state, &ClothesAction::Wash),
+        ClothesState::Tattered
+    )
 }
 
 #[test]
 fn sm_2_dry_clean_clothes() {
-
+    let starting_state = ClothesState::Clean(3);
+    assert_eq!(
+        ClothesMachine::next_state(&starting_state, &ClothesAction::Dry),
+        ClothesState::Clean(1)
+    )
 }
 
 #[test]
 fn sm_2_dry_dirty_clothes() {
-
+    let starting_state = ClothesState::Dirty(3);
+    assert_eq!(
+        ClothesMachine::next_state(&starting_state, &ClothesAction::Dry),
+        ClothesState::Dirty(0)
+    )
 }
 
 #[test]
 fn sm_2_dry_wet_clothes() {
-
+    let starting_state = ClothesState::Wet(3);
+    assert_eq!(
+        ClothesMachine::next_state(&starting_state, &ClothesAction::Dry),
+        ClothesState::Clean(2)
+    )
 }
 
 #[test]
 fn sm_2_dry_tattered_clothes() {
-
+    let starting_state = ClothesState::Tattered;
+    assert_eq!(
+        ClothesMachine::next_state(&starting_state, &ClothesAction::Dry),
+        ClothesState::Tattered
+    )
 }
